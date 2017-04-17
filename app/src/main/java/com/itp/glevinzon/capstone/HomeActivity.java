6892dc10a009f9ee83389490a0513e959fd0223b
Glevinzon Dapal <glevinzon.zeal@gmail.com>
2017-04-18 00:03:03 +0800

    work in progress speech

--- app/src/main/java/com/itp/glevinzon/capstone/HomeActivity.java
+++ app/src/main/java/com/itp/glevinzon/capstone/HomeActivity.java
@@ -1,13 +1,36 @@
 package com.itp.glevinzon.capstone;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import net.gotev.speech.GoogleVoiceTypingDisabledException;
+import net.gotev.speech.Speech;
+import net.gotev.speech.SpeechDelegate;
+import net.gotev.speech.SpeechRecognitionNotAvailable;
+import net.gotev.speech.SpeechUtil;
+import net.gotev.speech.TextToSpeechCallback;
+import net.gotev.speech.ui.SpeechProgressView;
+
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity implements SpeechDelegate {
+    private TextView text;
+    private SpeechProgressView progress;
+    private LinearLayout speechLayout;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +39,151 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        text = (TextView) findViewById(R.id.text);
+        progress = (SpeechProgressView) findViewById(R.id.progress);
+        speechLayout = (LinearLayout) findViewById(R.id.speech_layout);;
+
+        int[] colors = {
+                ContextCompat.getColor(this, android.R.color.black),
+                ContextCompat.getColor(this, android.R.color.darker_gray),
+                ContextCompat.getColor(this, android.R.color.black),
+                ContextCompat.getColor(this, android.R.color.holo_orange_dark),
+                ContextCompat.getColor(this, android.R.color.holo_red_dark)
+        };
+        progress.setColors(colors);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> onButtonClick());
+}
+
+    @Override
+    protected void onDestroy() {
+        Speech.getInstance().unregisterDelegate();
+    }
+
+    private void onButtonClick() {
+        if (Speech.getInstance().isListening()) {
+            Speech.getInstance().stopListening();
+        } else {
+            RxPermissions.getInstance(this)
+                    .request(Manifest.permission.RECORD_AUDIO)
+                    .subscribe(granted -> {
+                        if (granted) { // Always true pre-M
+                            onRecordAudioPermissionGranted();
+                        } else {
+                            Toast.makeText(HomeActivity.this, R.string.permission_required, Toast.LENGTH_LONG);
+                        }
+                    });
+        }
+    }
+
+    private void onRecordAudioPermissionGranted() {
+        fab.setVisibility(View.GONE);
+        speechLayout.setVisibility(View.VISIBLE);
+
+        try {
+            Speech.getInstance().stopTextToSpeech();
+            Speech.getInstance().startListening(progress, HomeActivity.this);
+
+        } catch (SpeechRecognitionNotAvailable exc) {
+            showSpeechNotSupportedDialog();
+
+        } catch (GoogleVoiceTypingDisabledException exc) {
+            showEnableGoogleVoiceTyping();
+        }
+    }
+
+    private void onSpeakClick() {
+        if (text.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, R.string.input_something, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Speech.getInstance().say(text.getText().toString().trim(), new TextToSpeechCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onStart() {
+                Toast.makeText(HomeActivity.this, "TTS onStart", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCompleted() {
+                Toast.makeText(HomeActivity.this, "TTS onCompleted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(HomeActivity.this, "TTS onError", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onStartOfSpeech() {
+    }
+
+    @Override
+    public void onSpeechRmsChanged(float value) {
+        //Log.d(getClass().getSimpleName(), "Speech recognition rms is now " + value +  "dB");
+    }
+
+    @Override
+    public void onSpeechResult(String result) {
+
+        fab.setVisibility(View.VISIBLE);
+        speechLayout.setVisibility(View.GONE);
+
+        text.setText(result);
+
+        if (result.isEmpty()) {
+            Speech.getInstance().say(getString(R.string.repeat));
+
+        } else {
+            Speech.getInstance().say(result);
+        }
+    }
+
+    @Override
+    public void onSpeechPartialResults(List<String> results) {
+        text.setText("");
+        for (String partial : results) {
+            text.append(partial + " ");
+        }
+    }
+
+    private void showSpeechNotSupportedDialog() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        SpeechUtil.redirectUserToGoogleAppOnPlayStore(HomeActivity.this);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.speech_not_available)
+                .setCancelable(false)
+                .setPositiveButton(R.string.yes, dialogClickListener)
+                .setNegativeButton(R.string.no, dialogClickListener)
+                .show();
+    }
+
+    private void showEnableGoogleVoiceTyping() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.enable_google_voice_typing)
+                .setCancelable(false)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // do nothing
+                    }
+                })
+                .show();
     }
 
 }
